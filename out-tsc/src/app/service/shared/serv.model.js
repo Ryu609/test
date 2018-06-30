d036d8286c75de31171688d4eb318e64d6300195
ryu609 <m.joynauth@gmail.com>
2018-06-30 17:03:49 +0400

    Auto-generated commit

--- out-tsc/src/app/service/shared/serv.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Serv = /** @class */ (function () {
-    function Serv() {
-    }
-    return Serv;
-}());
-exports.Serv = Serv;
-//# sourceMappingURL=serv.model.js.map
